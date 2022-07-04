6d7971dc6c3feecc88ebb3954b62ae3b992dc60a
ZakariyeAden <zakariye5417@gmail>
2022-07-04 11:59:16 -0500

    _

--- src/App.js
+++ src/App.js
@@ -1,16 +1,8 @@
-import React, { useState } from 'react'
+import React from 'react'
 import Quote from './Components/Quote';
 
-
-
 function App() {
-  
-  const [quote,setQuote] = useState();
-  const request = fetch('https://animechan.vercel.app/api/random')
-    .then(response => response.json())
-    .then(quote => console.log(quote))
 
-    console.log(request);
   return (
     <div>
       <Quote />
