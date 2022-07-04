997b0363c7f2bed9a0c4f708d85da4bdd0390de4
ZakariyeAden <zakariye5417@gmail>
2022-07-04 11:59:06 -0500

    it works but small issue when it renders in the page

--- src/Components/Quote.js
+++ src/Components/Quote.js
@@ -1,16 +1,42 @@
-import React from "react";
+import React, { useState,useEffect } from "react";
 import Card from "./Card";
+import classes from "./Style/Quote.module.css";
+
+function Quote() {
+
+  // fetch('https://animechan.vercel.app/api/random')
+  //   .then(response => response.json())
+  //   .then(quote => console.log(quote))
+
+  const [quotes, setQuotes] = useState("");
 
 
-function Quote(props) {
+  const getQuote = () => {
+    fetch("https://animechan.vercel.app/api/random")
+      .then(response => response.json())
+      .then(quote => {
+        setQuotes(quote);
+        console.log(quote)
+      });
+  };
+
+
+useEffect(() => {
+  getQuote();
+}, [])
+
   return (
     <Card>
-      <form>
-        <h1>{props.anime}Anime</h1>
-        <h4>{props.character}character</h4>
-        <p>{props.quote}Quote</p>
-        <button>Hello</button>
-      </form>
+      <div className={classes.form}>
+        <div>
+          <form>
+            <h2>{quotes.anime}</h2>
+            <p>{quotes.quote}</p>
+            <h6>{quotes.character}</h6>
+            <button onClick={getQuote}>Get Quote</button>
+          </form>
+        </div>
+      </div>
     </Card>
   );
 }
