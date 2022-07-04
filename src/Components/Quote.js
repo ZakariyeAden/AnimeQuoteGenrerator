4d22f206b612a37d8b4116256f53b45bcf3989ac
ZakariyeAden <zakariye5417@gmail>
2022-07-04 13:47:12 -0500

    adding delay to the two objects being sent although need to add loading text

--- src/Components/Quote.js
+++ src/Components/Quote.js
@@ -1,41 +1,44 @@
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Card from "./Card";
 import classes from "./Style/Quote.module.css";
 
 function Quote() {
-
-  // fetch('https://animechan.vercel.app/api/random')
-  //   .then(response => response.json())
-  //   .then(quote => console.log(quote))
-
   const [quotes, setQuotes] = useState("");
-
-
+ 
+  // getting two objects at once from the API
   const getQuote = () => {
     fetch("https://animechan.vercel.app/api/random")
       .then(response => response.json())
       .then(quote => {
         setQuotes(quote);
-        console.log(quote)
+
+        console.log();
       });
   };
 
+  //  Loading quote right away
+  useEffect(() => {
+    // Delay the objects
+    setTimeout(() => {
+      getQuote();
+    }, 1000);
+  }, [setQuotes]);
 
-useEffect(() => {
-  getQuote();
-}, [])
+  const form = (
+    <form>
+      <h2>{quotes.anime}</h2>
+      <p>{quotes.quote}</p>
+      <h6>{quotes.character}</h6>
+      <button onClick={getQuote}>Get Quote</button>
+    </form>
+  );
 
+  // add loading screen
+  const textScreen = <p>Loading...</p>;
   return (
     <Card>
-      <div className={classes.form}>
-        <div>
-          <form>
-            <h2>{quotes.anime}</h2>
-            <p>{quotes.quote}</p>
-            <h6>{quotes.character}</h6>
-            <button onClick={getQuote}>Get Quote</button>
-          </form>
-        </div>
+       <div className={classes.form}>
+          <div>{form}</div>
       </div>
     </Card>
   );
